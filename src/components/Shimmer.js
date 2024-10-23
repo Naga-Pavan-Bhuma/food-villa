08b149a830cc61f1e3006d8d07a1c6f999e462da
Naga-Pavan-Bhuma <bhumanagapavan@gmail.com>
2024-10-23 11:18:52 +0530

    Shimmer UI Changed

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,11 +2,23 @@ import React from "react";
 import "./Shimmer.css"; 
 const Shimmer = () => {
   return (
+    <>
+    <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+        />
+        <button className="search-btn">
+          Search
+        </button>
+      </div>
     <div className="shimmer-container">
       {Array(10).fill("").map((_, index) => (
         <div className="shimmer-card" key={index}>
           <div className="shimmer-image"></div>
           <div className="shimmer-content">
+            <div className="shimmer-line shimmer-line-heading"></div>
             <div className="shimmer-line shimmer-line-short"></div>
             <div className="shimmer-line shimmer-line-long"></div>
             <div className="shimmer-line shimmer-line-medium"></div>
@@ -14,6 +26,7 @@ const Shimmer = () => {
         </div>
       ))}
     </div>
+    </>
   );
 };
 
